dc9248dd8c1d40f1915ee00319afe0f6c807f4f5
Mubasshir Nakib <mubasshirnakib@gmail.com>
2026-01-18 23:46:01 +0600

    new code

--- Auth/HTML/login.php
+++ Auth/HTML/login.php
@@ -7,4 +7,43 @@
     <link rel="stylesheet" href="../../Shared/CSS/navbar.css">
 </head>
 <body>
+      
+    <?php include '../../Shared/HTML/navbar.php'; ?>
     
+    <div class="login-wrapper">
+        <div class="login-box">
+            <div class="login-header">
+                <h2>Welcome Back</h2>
+                <p>Please login to continue</p>
+            </div>
+
+            <form action="../PHP/loginProcess.php" method="POST">
+                
+                <div class="input-group">
+                    <label>Email Address</label>
+                    <input type="email" name="email" placeholder="Enter your email" required>
+                </div>
+
+                <div class="input-group">
+                    <label>Password</label>
+                    <input type="password" name="password" placeholder="Enter your password" required>
+                </div>
+                
+                <div class="options">
+                    <label class="remember-me">
+                        <input type="checkbox" name="remember"> 
+                        <span>Remember Me</span>
+                    </label>
+                    </div>
+                
+                <button type="submit" name="login" class="btn-login">LOGIN</button>
+
+                <div class="register-link">
+                    <p>Don't have an account? <a href="register.php">Register Here</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
