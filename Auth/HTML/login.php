49f1e4eee2a6f7f7869d7aeb6082f5e321224ef7
Mubasshir Nakib <mubasshirnakib@gmail.com>
2026-01-18 23:39:12 +0600

    Add login page structure 2

--- Auth/HTML/login.php
+++ Auth/HTML/login.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login Page</title>
+    <title>Login - EventSys</title>
+    <link rel="stylesheet" href="../CSS/login.css">
+    <link rel="stylesheet" href="../../Shared/CSS/navbar.css">
 </head>
 <body>
-
-    <h1>Hello World!</h1>
-    <p>Page.</p>
-
-</body>
-</html>
+    
